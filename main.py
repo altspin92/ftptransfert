50b4379a6bb5888d3f8dcb15639b2a6b8cbdf7b5
Emanuele Beccaria <34138916+altspin92@users.noreply.github.com>
2024-10-23 14:44:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import json
+import os
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLineEdit, QLabel,
                              QFileDialog, QMessageBox, QDialog, QFormLayout, QHBoxLayout,
-                             QVBoxLayout, QGridLayout, QRadioButton, QComboBox, QPlainTextEdit)
+                             QVBoxLayout, QGridLayout, QRadioButton, QComboBox, QPlainTextEdit, QCheckBox)
 from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon  # Importa QIcon per gestire l'icona della finestra
 from datetime import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -73,7 +75,9 @@ class MainWindow(QWidget):
         super().__init__()
         MainWindow.open_windows.append(self)
         self.setWindowTitle("FTP Bizpal")
+        self.setWindowIcon(QIcon('logo.jpg'))  # Imposta l'icona della finestra con il logo personalizzato
         self.email_settings = {}
+        self.existing_files = set()  # This will hold the initial files in the directory
         self.initUI()
         self.setupTimer()
 
@@ -81,14 +85,19 @@ class MainWindow(QWidget):
         grid = QGridLayout()
         self.setLayout(grid)
 
+        # Radio buttons for transfer type
         self.direction_group = QHBoxLayout()
         self.to_remote_button = QRadioButton("Local to Remote")
         self.to_remote_button.setChecked(True)
         self.to_local_button = QRadioButton("Remote to Local")
+        self.to_local_local_button = QRadioButton("Local to Local")  # New button for local to local
+
         self.direction_group.addWidget(self.to_remote_button)
         self.direction_group.addWidget(self.to_local_button)
+        self.direction_group.addWidget(self.to_local_local_button)  # Add new button to layout
         grid.addLayout(self.direction_group, 0, 0, 1, 3)
 
+        # SFTP configuration inputs
         self.host_label = QLabel("SFTP Host:")
         self.host_line_edit = QLineEdit()
         self.port_label = QLabel("SFTP Port:")
@@ -99,6 +108,17 @@ class MainWindow(QWidget):
         self.password_line_edit = QLineEdit()
         self.password_line_edit.setEchoMode(QLineEdit.Password)
 
+        grid.addWidget(self.host_label, 1, 0)
+        grid.addWidget(self.host_line_edit, 1, 1, 1, 2)  # Spanning across two columns
+        grid.addWidget(self.port_label, 2, 0)
+        grid.addWidget(self.port_line_edit, 2, 1, 1, 2)
+
+        grid.addWidget(self.username_label, 3, 0)
+        grid.addWidget(self.username_line_edit, 3, 1, 1, 2)
+        grid.addWidget(self.password_label, 4, 0)
+        grid.addWidget(self.password_line_edit, 4, 1, 1, 2)
+
+        # Local and Remote Directory inputs
         self.local_dir_label = QLabel("Local Directory:")
         self.local_dir_line_edit = QLineEdit()
         self.local_dir_button = QPushButton("Choose...")
@@ -106,10 +126,22 @@ class MainWindow(QWidget):
 
         self.remote_dir_label = QLabel("Remote Directory:")
         self.remote_dir_line_edit = QLineEdit()
+
+        grid.addWidget(self.local_dir_label, 5, 0)
+        grid.addWidget(self.local_dir_line_edit, 5, 1)
+        grid.addWidget(self.local_dir_button, 5, 2)
+
+        grid.addWidget(self.remote_dir_label, 6, 0)
+        grid.addWidget(self.remote_dir_line_edit, 6, 1, 1, 2)
+
+        # Action buttons
         self.sync_button = QPushButton("Sync Now")
         self.sync_button.clicked.connect(self.sync_files)
         self.test_connection_button = QPushButton("Test Connection")
         self.test_connection_button.clicked.connect(self.test_connection)
+
+        grid.addWidget(self.sync_button, 7, 0)
+        grid.addWidget(self.test_connection_button, 7, 1, 1, 2)
 
         self.emailSettingsButton = QPushButton("Email Settings")
         self.emailSettingsButton.clicked.connect(self.openEmailSettingsDialog)
@@ -120,48 +152,46 @@ class MainWindow(QWidget):
         self.loadConfigButton = QPushButton("Load Configuration")
         self.loadConfigButton.clicked.connect(self.load_configuration)
 
-        self.timerLabel = QLabel("Timer Interval:")
-        self.timerComboBox = QComboBox()
-        self.timerComboBox.addItems(["1 minutes", "5 minutes", "10 minutes", "30 minutes", "1 hour", "2 hours"])
-        self.timerComboBox.currentIndexChanged.connect(self.update_timer_interval)
-
-        self.log_window = QPlainTextEdit()
-        self.log_window.setReadOnly(True)
-        self.clearLogButton = QPushButton("Clear Logs")
-        self.clearLogButton.clicked.connect(self.clear_logs)
-
-        grid.addWidget(self.host_label, 1, 0)
-        grid.addWidget(self.host_line_edit, 1, 1)
-        grid.addWidget(self.port_label, 2, 0)
-        grid.addWidget(self.port_line_edit, 2, 1)
-        grid.addWidget(self.username_label, 3, 0)
-        grid.addWidget(self.username_line_edit, 3, 1)
-        grid.addWidget(self.password_label, 4, 0)
-        grid.addWidget(self.password_line_edit, 4, 1)
-        grid.addWidget(self.local_dir_label, 5, 0)
-        grid.addWidget(self.local_dir_line_edit, 5, 1)
-        grid.addWidget(self.local_dir_button, 5, 2)
-        grid.addWidget(self.remote_dir_label, 6, 0)
-        grid.addWidget(self.remote_dir_line_edit, 6, 1)
-        grid.addWidget(self.sync_button, 7, 0)
-        grid.addWidget(self.test_connection_button, 7, 1)
         grid.addWidget(self.emailSettingsButton, 8, 0)
         grid.addWidget(self.newWindowButton, 8, 1)
         grid.addWidget(self.saveConfigButton, 8, 2)
+
         grid.addWidget(self.loadConfigButton, 9, 0)
+
+        # Timer and log controls
+        self.delete_after_transfer_checkbox = QCheckBox("Delete files after transfer")
+        self.transfer_new_files_only_checkbox = QCheckBox("Transfer only new files")
+
+        self.timerLabel = QLabel("Timer Interval:")
+        self.timerComboBox = QComboBox()
+        self.timerComboBox.addItems([
+            "1 minute", "5 minutes", "10 minutes", "30 minutes", "1 hour", "2 hours",
+            "3 hours", "5 hours", "6 hours", "10 hours", "12 hours", "18 hours", "24 hours"
+        ])
+        self.timerComboBox.currentIndexChanged.connect(self.update_timer_interval)
+
         grid.addWidget(self.timerLabel, 9, 1)
         grid.addWidget(self.timerComboBox, 9, 2)
-        grid.addWidget(self.clearLogButton, 10, 0)
-        grid.addWidget(self.log_window, 11, 0, 1, 3)
+        grid.addWidget(self.delete_after_transfer_checkbox, 10, 0)
+        grid.addWidget(self.transfer_new_files_only_checkbox, 10, 1)
 
-    def open_new_window(self):
-        new_win = MainWindow()
-        new_win.show()
+        self.clearLogButton = QPushButton("Clear Logs")
+        self.clearLogButton.clicked.connect(self.clear_logs)
+
+        self.log_window = QPlainTextEdit()
+        self.log_window.setReadOnly(True)
+
+        grid.addWidget(self.clearLogButton, 10, 2)
+        grid.addWidget(self.log_window, 11, 0, 1, 3)
 
     def setupTimer(self):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.sync_files)
         self.timer.start(300000)  # Default to 5 minutes
+
+    def open_new_window(self):  # Aggiunta la funzione per aprire una nuova finestra
+        new_window = MainWindow()
+        new_window.show()
 
     def openEmailSettingsDialog(self):
         dialog = EmailSettingsDialog(self)
@@ -172,12 +202,19 @@ class MainWindow(QWidget):
     def update_timer_interval(self):
         interval_text = self.timerComboBox.currentText()
         intervals = {
-            "1 minutes": 60000,
+            "1 minute": 60000,
             "5 minutes": 300000,
             "10 minutes": 600000,
             "30 minutes": 1800000,
             "1 hour": 3600000,
-            "2 hours": 7200000
+            "2 hours": 7200000,
+            "3 hours": 10800000,
+            "5 hours": 18000000,
+            "6 hours": 21600000,
+            "10 hours": 36000000,
+            "12 hours": 43200000,
+            "18 hours": 64800000,
+            "24 hours": 86400000
         }
         self.timer.start(intervals[interval_text])
         self.append_log(f"Timer interval set to {interval_text}")
@@ -191,25 +228,70 @@ class MainWindow(QWidget):
 
     def sync_files(self):
         self.append_log("Starting synchronization...")
-        direction = "to_remote" if self.to_remote_button.isChecked() else "to_local"
+        direction = "to_remote" if self.to_remote_button.isChecked() else ("to_local" if self.to_local_button.isChecked() else "local_to_local")
+
         try:
-            sftp_client = SftpClient(self.host_line_edit.text(), int(self.port_line_edit.text()),
-                                     self.username_line_edit.text(), self.password_line_edit.text(),
-                                     self.append_log)
-            sftp_client.connect()
-            if direction == "to_remote":
-                files_transferred = sftp_client.upload_from_local_to_remote(self.local_dir_line_edit.text(),
-                                                                            self.remote_dir_line_edit.text())
+            if direction == "local_to_local":
+                self.local_to_local_transfer()
             else:
-                files_transferred = sftp_client.synchronize_and_clear_remote(self.remote_dir_line_edit.text(),
-                                                                             self.local_dir_line_edit.text())
-            sftp_client.close()
-            if files_transferred:
-                self.append_log(f"Files transferred: {', '.join(files_transferred)}")
-                self.send_email(files_transferred, direction)
+                if self.transfer_new_files_only_checkbox.isChecked():
+                    self.sync_only_new_files(direction)
+                else:
+                    self.perform_sync(direction)
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
             self.append_log(f"Error: {e}")
+
+    def perform_sync(self, direction):
+        sftp_client = SftpClient(self.host_line_edit.text(), int(self.port_line_edit.text()),
+                                 self.username_line_edit.text(), self.password_line_edit.text(),
+                                 self.append_log)
+        sftp_client.connect()
+        if direction == "to_remote":
+            files_transferred = sftp_client.upload_from_local_to_remote(self.local_dir_line_edit.text(),
+                                                                        self.remote_dir_line_edit.text())
+        else:
+            files_transferred = sftp_client.synchronize_and_clear_remote(self.remote_dir_line_edit.text(),
+                                                                         self.local_dir_line_edit.text())
+        sftp_client.close()
+        if files_transferred:
+            self.append_log(f"Files transferred: {', '.join(files_transferred)}")
+            self.send_email(files_transferred, direction)
+        
+        if self.delete_after_transfer_checkbox.isChecked():
+            for file in files_transferred:
+                self.append_log(f"Deleting file {file} from source directory.")
+
+    def sync_only_new_files(self, direction):
+        self.append_log("Transferring only new files added to the local directory...")
+        src_dir = self.local_dir_line_edit.text()
+        current_files = set(os.listdir(src_dir))
+        new_files = current_files - self.existing_files
+        if new_files:
+            self.append_log(f"New files detected: {', '.join(new_files)}")
+            if direction == "to_remote":
+                self.perform_sync(direction)
+        else:
+            self.append_log("No new files found to transfer.")
+        self.existing_files = current_files
+
+    def local_to_local_transfer(self):
+        try:
+            src_dir = self.local_dir_line_edit.text()
+            dest_dir = self.remote_dir_line_edit.text()
+            self.append_log(f"Transferring files from {src_dir} to {dest_dir}...")
+
+            files_transferred = os.listdir(src_dir)  # Transferring all files from src to dest
+            self.append_log(f"Files transferred: {', '.join(files_transferred)}")
+
+            if self.delete_after_transfer_checkbox.isChecked():
+                for file in files_transferred:
+                    os.remove(os.path.join(src_dir, file))
+                    self.append_log(f"Deleting file {file} from {src_dir}")
+
+            self.append_log("Local to local transfer complete.")
+        except Exception as e:
+            self.append_log(f"Error during local to local transfer: {e}")
 
     def test_connection(self):
         try:
@@ -266,7 +348,9 @@ class MainWindow(QWidget):
                 "local_dir": self.local_dir_line_edit.text(),
                 "remote_dir": self.remote_dir_line_edit.text(),
                 "email_settings": self.email_settings,
-                "direction": "to_remote" if self.to_remote_button.isChecked() else "to_local"
+                "direction": "to_remote" if self.to_remote_button.isChecked() else ("to_local" if self.to_local_button.isChecked() else "local_to_local"),
+                "delete_after_transfer": self.delete_after_transfer_checkbox.isChecked(),
+                "transfer_new_files_only": self.transfer_new_files_only_checkbox.isChecked()
             }
             with open(path, 'w') as file:
                 json.dump(config, file, indent=4)
@@ -286,8 +370,12 @@ class MainWindow(QWidget):
             self.email_settings = config.get('email_settings', {})
             if config.get('direction') == "to_remote":
                 self.to_remote_button.setChecked(True)
+            elif config.get('direction') == "to_local":
+                self.to_local_button.setChecked(True)
             else:
-                self.to_local_button.setChecked(False)
+                self.to_local_local_button.setChecked(True)
+            self.delete_after_transfer_checkbox.setChecked(config.get('delete_after_transfer', False))
+            self.transfer_new_files_only_checkbox.setChecked(config.get('transfer_new_files_only', False))
             self.append_log("Configuration loaded from " + path)
 
 if __name__ == "__main__":
